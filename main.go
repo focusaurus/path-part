5088c8b7e382c7987b9f520d08be691da893ca06
Peter Lyons <pete.lyons@mailchimp.com>
2020-07-13 06:15:43 -0600

    Add crude initial implementation of most transforms

--- main.go
+++ main.go
@@ -2,17 +2,42 @@ package main
 
 import (
 	"bufio"
-	"path/filepath"
+	"flag"
 	"fmt"
 	"os"
+	"path/filepath"
+	"strings"
 )
 
+// Given a typical path, here's our terminology
+// some/directories/info.dat.zip
+// some/directories is the "path"
+// "info.dat.zip" is the "name"
+// "zip" is the "extension"
+// "dat.zip" is the "extensions"
+// "info" is the "base"
 func main() {
-
+	flag.Parse()
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
-		base := filepath.Base(scanner.Text())
-		fmt.Println(base)
+		line := scanner.Text()
+		for _, arg := range flag.Args() {
+			switch arg {
+			case "extensions":
+				line = filepath.Base(line)
+				line = strings.SplitN(line, ".", 2)[1]
+			case "extension":
+				line = strings.TrimLeft(filepath.Ext(line), ".")
+			case "last":
+				line = filepath.Base(line)
+			case "path":
+				line = filepath.Dir(line)
+			case "base":
+				line = filepath.Base(line)
+				line = strings.SplitN(line, ".", 2)[0]
+			}
+		}
+		fmt.Println(line)
 	}
 
 	if err := scanner.Err(); err != nil {
